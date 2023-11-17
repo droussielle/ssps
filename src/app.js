17e264e37331b6dd111a5720c7d3ec9ab0228a20
takhoa1805 <khoa.tran1805@hcmut.edu.vn>
2023-11-17 22:59:13 +0700

    cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const {
   account,
   student,
@@ -16,6 +17,14 @@ module.exports = async (app) => {
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
+  // app.use(cors());
+  app.use(
+    cors({
+      origin: "http://localhost:5500",
+      methods: "GET,POST,PUT,DELETE",
+      credentials: true,
+    })
+  );
 
   app.use('/account', account);
   app.use('/student', student);
@@ -23,6 +32,17 @@ module.exports = async (app) => {
   app.use('/spso', spso);
   app.use('/printer', printer);
   app.use('/printorder', printorder);
+  
+//   app.use((req,res,next)=>{
+//     res.header('Access-Control-Allow-Origin','*');//give access to specific client, in this case is any client "*"
+//     res.header('Access-Control-Allow-Headers',
+//                 'Origin, X-Requested-With,Content-Type, Accept, Authorization');//give access to specific header
+//     if(req.method === 'OPTIONS'){ //set allowed methods
+//         res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+//         return res.status(200).json('Okay');
+//     }
+//     next();
+// });
 
   app.use((req, res, next) => {
     const error = new Error('Not found');
