aef1c0e1df1e9eefd73dc2dd78575ad799bd7fa7
Thái Ngọc Rạng <98004370+restom0@users.noreply.github.com>
2023-11-17 21:10:29 +0700

    add buy, history and update manage.js

---
+++ demo/src/scripts/buy.js
@@ -0,0 +1,48 @@
+var data = [
+    {
+        'page': 5,
+        'cost': 1000
+    }, {
+        'page': 10,
+        'cost': 2000
+    }, {
+        'page': 15,
+        'cost': 1500
+    }, {
+        'page': 20,
+        'cost': 2000
+    }, {
+        'page': 25,
+        'cost': 2500
+    }];
+$(document).ready(function () {
+    loadData();
+});
+
+function loadData() {
+    var str = `<p class="text-xl font-bold">Mua thêm trang</p>
+    <p>Chọn số trang cần mua</p>`;
+    data.forEach((el, index) => {
+        str += `<div class="flex flex-row justify-between" id="buy-item">
+        <div class="flex flex-row items-center space-x-2">
+          <input id="default-radio-1" type="radio" value="" name="default-radio"
+            class="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600" />
+          <p>`+ el['page'] + ` trang</p>
+        </div>
+        <div class="flex flex-row items-center space-x-2">
+          <p>`+ el['cost'].toLocaleString('de-DE') + `₫</p>
+        </div>
+      </div>`;
+    })
+    str += `<div class="flex w-full flex-row items-center justify-between pb-2">
+    <button type="button"
+      class="items inline-flex gap-x-3 rounded-full px-6 py-2.5 text-center text-black shadow-1 transition ease-out hover:shadow-3 active:bg-gray-200">
+      Hủy
+    </button>
+    <button type="button"
+      class="items inline-flex gap-x-1 rounded-full bg-blue-primary px-5 py-2.5 text-center text-white shadow-1 transition ease-out hover:bg-[#0d3eba] hover:shadow-3 active:bg-[#032478]">
+      Tiếp tục
+    </button>
+  </div>`
+    $('#buy-pages').html(str);
+}
--- demo/src/scripts/manage.js
+++ demo/src/scripts/manage.js
@@ -14,12 +14,12 @@ $('#add-printer').on('click', function () {
   $('#printer-name').text('Máy in: ' + printer1.name);
   $('#manage-printer-body').html(
     '<p class="text-sm font-bold text-gray-600">Chi tiết</p><hr class="border w-full" /><div class="flex flex-row justify-between"><p>Trạng thái</p><p>' +
-      printer1.status +
-      '</p></div><div class="flex flex-row justify-between"><p>Chế độ</p><p>' +
-      printer1.mode +
-      '</p></div><div class="flex flex-row justify-between"><p>Thời gian hoạt động</p><p>' +
-      printer1.uptimeSince +
-      '</p></div>',
+    printer1.status +
+    '</p></div><div class="flex flex-row justify-between"><p>Chế độ</p><p>' +
+    printer1.mode +
+    '</p></div><div class="flex flex-row justify-between"><p>Thời gian hoạt động</p><p>' +
+    printer1.uptimeSince +
+    '</p></div>',
   );
   $('#manage-printer-actions').html(
     '<button type="button" class="transition ease-out px-6 py-2.5 shadow-1 text-black inline-flex rounded-full text-center gap-x-3 items hover:shadow-3 hover:bg-button-hover active:bg-button-active" >Báo lỗi</button><button type="button" class="transition ease-out px-6 py-2.5 shadow-1 text-black inline-flex rounded-full text-center gap-x-3 items hover:shadow-3 hover:bg-button-hover active:bg-button-active">Xem nhật kí hoạt động</button><button type="button" class="transition ease-out px-5 py-2.5 shadow-1 text-white inline-flex items rounded-full text-center gap-x-1 bg-red-700 hover:shadow-3 hover:bg-red-600 active:bg-red-800">Vô hiệu hóa</button>',
@@ -47,3 +47,10 @@ $('#stop-print').on('click', function () {
     );
   $('#pause-print').addClass('invisible');
 });
+$(document).ready(function () {
+  loadPrinterQueue();
+});
+function loadPrinterQueue() {
+  let str = ''
+  str+=``
+}
