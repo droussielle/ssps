befbded527694a1869a7453adbaabb9e530a71f2
Daniel Roussielle <9989840+droussielle@users.noreply.github.com>
2023-11-29 08:40:56 +0700

    new postman and db name

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 async function connect() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/hcmut_spss');
+    await mongoose.connect('mongodb://127.0.0.1:27017/ssps');
     console.log('Connect success');
   } catch (error) {
     console.log('Connect failed');
