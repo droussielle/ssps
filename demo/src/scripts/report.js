3d072a5bbb69a0dcb5ff9e2cdc4a582f054ea955
Thái Ngọc Rạng <98004370+restom0@users.noreply.github.com>
2023-12-02 01:26:17 +0700

    update api

--- demo/src/scripts/report.js
+++ demo/src/scripts/report.js
@@ -1,9 +1,11 @@
 // nothing in here yet
 /* eslint-disable require-jsdoc */
+import $, { ajax } from 'jquery';
+import jQuery from 'jquery'
 $(() => {
   // login()
   // setTimeout(() => {
-  //   deletePrinter();
+  // deletePrinter();
   // }, 1000)
   // changeStatusPrinter();
   // addPrinter();
@@ -14,202 +16,245 @@ $(() => {
   // buyCredit();
   // setDefault();
   // getStaff();
+  // loadData();
+  // getPrintOrder();
+  // getQueue();
+  // printNextFile();
+  uploadFile();
 });
-function login() {
-  $.ajax({
-    type: 'POST',
-    url: 'http://localhost:3000/account/login',
-    data: {
-      email: 'khoa.tran1805@hcmut.edu.vn',
-      password: 'khoadeptrai',
-    },
-    dataType: 'JSON',
-    success: function (res) {
-      if (!res.error) {
-        console.log('Đăng nhập thành công');
-        localStorage.setItem('token', res.token);
-      } else {
-        console.log(res.error.msg);
-      }
-    },
-  });
-}
+jQuery.each(["put", "delete", "patch"], function (i, method) {
+  jQuery[method] = function (url, data, callback, type) {
+    if (jQuery.isFunction(data)) {
+      type = type || callback;
+      callback = data;
+      data = undefined;
+    }
+
+    return jQuery.ajax({
+      url: url,
+      type: method,
+      dataType: type,
+      data: data,
+      success: callback
+    });
+  };
+});
 function loadPrinter() {
-  $.ajax({
-    type: 'get',
-    url: 'http://localhost:3000/spso/printer',
-    headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
-    dataType: 'JSON',
-    success: function (res) {
-      if (!res.error) {
-        console.log(res.data);
-      } else {
-        console.log(res.error.msg);
-      }
-    },
-  });
+  $.get('http://localhost:3000/spso/printer')
+    .done(function (data) {
+      console.log(data.data);
+      const printers = data.data;
+      printers.forEach((el) => {
+        el._id
+        el.brand
+        el.model
+        el.permittedFileType
+        el.printerStatus
+        el.shortDescription
+      });
+    })
+    .fail(function (xhr, status, error) {
+      console.log(xhr.status);
+      console.log(xhr.responseJSON.error.message);
+    });
 }
 function addPrinter() {
-  $.ajax({
-    type: 'POST',
-    url: 'http://localhost:3000/spso/printer',
-    headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
-    data: {
-      brand: 'Cannon',
-      model: 'unknown',
-      shortDescription: 'Running just fine',
-      location: 'H6-609',
-      printerStatus: 'true',
-    },
-    dataType: 'JSON',
-    success: function (res) {
-      if (!res.error) {
-        console.log(res.data);
-      } else {
-        console.log(res.error.msg);
-      }
-    },
-  });
+  let data = {
+    brand: 'Cannon',
+    model: 'unknown',
+    shortDescription: 'Running just fine',
+    location: 'H6-609',
+    printerStatus: 'true',
+  }
+  $.post('http://localhost:3000/spso/printer', data)
+    .done(function (data) {
+      console.log(data.data);
+    })
+    .fail(function (xhr, status, error) {
+      console.log(xhr.status);
+      console.log(xhr.responseJSON.error.message);
+    });
 }
+
 function changeStatusPrinter() {
-  const id = '655cc41941b26a2ed8c9a6c8';
-  $.ajax({
-    type: 'patch',
-    url: 'http://localhost:3000/spso/printer/' + id,
-    headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
-    data: {
-      status: 'false',
-    },
-    dataType: 'JSON',
-    success: function (res) {
-      if (!res.error) {
-        console.log(res.data);
-      } else {
-        console.log(res.error.msg);
-      }
-    },
-  });
+  const id = "6569b65332e4f48b9382cebd"
+  const data = { status: false }
+  $.patch('http://localhost:3000/spso/printer/' + id, data)
+    .done(function (data) {
+      console.log(data.data.message);
+    })
+    .fail(function (xhr, status, error) {
+      console.log(xhr.status);
+      console.log(xhr.responseJSON.error.message);
+    });
 }
 function deletePrinter() {
-  const id = '655cb75ac7a7794fb5bfbc3d';
-  $.ajax({
-    type: 'delete',
-    url: 'http://localhost:3000/spso/printer/' + id,
-    headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
-    dataType: 'JSON',
-    success: function (res) {
-      if (!res.error) {
-        console.log(res.data);
-      } else {
-        console.log(res.error.msg);
-      }
-    },
-  });
+  const id = '656a13c7fef78db80c56a942';
+  $.delete('http://localhost:3000/spso/printer/' + id)
+    .done(function (data) {
+      console.log(data.message);
+    })
+    .fail(function (xhr, status, error) {
+      console.log(xhr.status);
+      console.log(xhr.responseJSON.error.message);
+    });
 }
 function getPrinter() {
-  const id = '655cc41941b26a2ed8c9a6c8';
-  $.ajax({
-    type: 'get',
-    url: 'http://localhost:3000/spso/printer/' + id,
-    headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
-    dataType: 'JSON',
-    success: function (res) {
-      if (!res.error) {
-        console.log(res.data);
-      } else {
-        console.log(res.error.msg);
-      }
-    },
-  });
+  const id = '6569b65332e4f48b9382cebd';
+  $.get('http://localhost:3000/spso/printer/' + id)
+    .done(function (data) {
+      console.log(data.data);
+      const printer = data.data;
+      printer._id
+      printer.brand
+      printer.model
+      printer.permittedFileType
+      printer.printerStatus
+      printer.shortDescription
+    })
+    .fail(function (xhr, status, error) {
+      console.log(xhr.status);
+      console.log(xhr.responseJSON.error.message);
+    });
 }
 function getStudent() {
   const id = '2113773';
-  $.ajax({
-    type: 'get',
-    url: 'http://localhost:3000/spso/student/' + id,
-    headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
-    dataType: 'JSON',
-    success: function (res) {
-      if (!res.error) {
-        console.log(res.data);
-      } else {
-        console.log(res.error.msg);
-      }
-    },
-  });
+  $.get('http://localhost:3000/spso/student/' + id)
+    .done(function (data) {
+      console.log(data.data);
+    })
+    .fail(function (xhr, status, error) {
+      console.log(xhr.status);
+      console.log(xhr.responseJSON.error.message);
+    });
 }
 function changePassword() {
-  var oldPass = 'khoadep';
-  var newPass = 'khoadeptrai';
-  $.ajax({
-    type: 'post',
-    url: 'http://localhost:3000/account/change-password',
-    headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
-    data: {
-      oldPassword: oldPass,
-      newPassword: newPass,
-    },
-    dataType: 'JSON',
-    success: function (res) {
-      if (!res.error) {
-        console.log(res.data);
-      } else {
-        console.log(res.error.message);
-      }
-    },
-  });
+  let data = {
+    oldPassword: 'khoadeptrai',
+    newPassword: 'khoadeptrai',
+  }
+  $.post('http://localhost:3000/account/change-password', data)
+    .done(function (data) {
+      console.log(data.acknowledged);
+    })
+    .fail(function (xhr, status, error) {
+      console.log(xhr.status);
+      console.log(xhr.responseJSON.error.message);
+    });
 }
 function buyCredit() {
-  var increment = 5;
-  $.ajax({
-    type: 'post',
-    url: 'http://localhost:3000/account/buy-credit',
-    headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
-    data: {
-      increment: increment,
-    },
-    dataType: 'JSON',
-    success: function (res) {
-      if (!res.error) {
-        console.log(res.data);
-      } else {
-        console.log(res.error.message);
-      }
-    },
-  });
+  let data = {
+    increment: 5
+  }
+  $.post('http://localhost:3000/account/buy-credit', data)
+    .done(function (data) {
+      console.log(data.message);
+    })
+    .fail(function (xhr, status, error) {
+      console.log(xhr.status);
+      console.log(xhr.responseJSON.error.message);
+    });
 }
 function setDefault() {
-  var amount = 5;
-  $.ajax({
-    type: 'post',
-    url: 'http://localhost:3000/spso/set-default-pages',
-    headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
-    data: {
-      amount: amount,
-    },
-    dataType: 'JSON',
-    success: function (res) {
-      if (!res.error) {
-        console.log(res.data);
-      } else {
-        console.log(res.error.message);
-      }
-    },
-  });
+  let data = {
+    amount: 5
+  }
+  $.post('http://localhost:3000/spso/set-default-pages', data)
+    .done(function (data) {
+      console.log(data.data.acknowledged);
+    })
+    .fail(function (xhr, status, error) {
+      console.log(xhr.status);
+      console.log(xhr.responseJSON.error.message);
+    });
 }
 function getStaff() {
-  const id = '2113773';
-  $.ajax({
-    type: 'get',
-    url: 'http://localhost:3000/spso/staff/' + id,
-    headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
-    dataType: 'JSON',
-    success: function (res) {
-      if (!res.error) {
-        console.log(res.data);
-      } else {
-        console.log(res.error.msg);
-      }
-    },
-  });
+  const id = '2222222';
+  $.get('http://localhost:3000/spso/staff/' + id)
+    .done(function (data) {
+      console.log(data.data);
+    })
+    .fail(function (xhr, status, error) {
+      console.log(xhr.status);
+      console.log(xhr.responseJSON.error.message);
+    });
+}
+function loadData() {
+  $.get('http://localhost:3000/spso/all-orders')
+    .done(function (data) {
+      console.log(data);
+    })
+    .fail(function (xhr, status, error) {
+      console.log(xhr.status);
+      console.log(xhr.responseJSON.error.message);
+    });
+}
+function getPrintOrder() {
+  const id = "655f1261056a63b60bafa897"
+  $.get('http://localhost:3000/spso/printorders/' + id)
+    .done(function (data) {
+      console.log(data);
+    })
+    .fail(function (xhr, status, error) {
+      console.log(xhr.status);
+      console.log(xhr.responseJSON.error.message);
+    });
+}
+function getQueue() {
+  const data = {
+    printer_id: "6569b65332e4f48b9382cebd"
+  }
+  $.get('http://localhost:3000/queue/all', { "printer_id": "6569b65332e4f48b9382cebd" })
+    .done(function (data) {
+      console.log(data);
+    })
+    .fail(function (xhr, status, error) {
+      console.log(xhr.status);
+      console.log(xhr.responseJSON.error.message);
+    });
+}
+function printNextFile() {
+  const data = {
+    printer_id: "6569b65332e4f48b9382cebd"
+  }
+  $.post('http://localhost:3000/queue/next', data)
+    .done(function (data) {
+      console.log(data);
+    })
+    .fail(function (xhr, status, error) {
+      console.log(xhr.status);
+      console.log(xhr.responseJSON.error.message);
+    });
+}
+function uploadFile() {
+  var data = {
+    printer: "6560174d32c3b94c3911986f",
+    note: "some note",
+    fileName: "khoa.pdf",
+    printProperties: {
+      paperSize: "a4",
+      numberOfPages: "4",
+      sided: "true",
+      copies: "2"
+
+    }
+  }
+  $.post('http://localhost:3000/printorder/', data)
+    .done(function (data) {
+      console.log(data);
+    })
+    .fail(function (xhr, status, error) {
+      console.log(xhr.status);
+      console.log(xhr.responseJSON.error.message);
+    });
+  const id = "65605142b7c29a6c8eb456a3"
+  data = [];
+  $.post('localhost:3000/printorder/upload/' + id, data)
+    .done(function (data) {
+      console.log(data);
+    })
+    .fail(function (xhr, status, error) {
+      console.log(xhr.status);
+      console.log(xhr.responseJSON.error.message);
+    });
 }
